37b69ef06eecc9fc9eadb0c1f4cd44cfd9d6b7ca
Austin Mroz <austin@comfy.org>
2025-11-26 10:18:35 -0800

    Fix linting

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -1316,7 +1316,7 @@ export class ComfyApp {
       workflow,
       this.rootGraph.serialize() as unknown as ComfyWorkflowJSON
     )
-    useSubgraphNavigationStore().updateHash()
+    void useSubgraphNavigationStore().updateHash()
     requestAnimationFrame(() => {
       this.canvas.setDirty(true, true)
     })
